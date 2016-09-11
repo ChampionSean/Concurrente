e9a23029703160e12ecb73bfcaffb34302b6ab3f
Gerardo <Gerardo@DESKTOP-LTM86GB.lan>
2016-09-10 22:19:48 -0500

    comentarios

--- src/practiconcu/PractiConcu.java
+++ src/practiconcu/PractiConcu.java
@@ -28,8 +28,6 @@ public class PractiConcu extends JPanel
     private final JTextArea parar;
     //Tarea a supervisar
     private Task tarea;
-    //Thread sobre el cual se realiza el proceso
-    private Thread t;
     
     class Task extends SwingWorker<Void, Void> {
 
@@ -112,16 +110,13 @@ public class PractiConcu extends JPanel
     }
 
     private static void createAndShowGUI() {
-        //Create and set up the window.
         JFrame frame = new JFrame("TareaMonitor");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //Create and set up the content pane.
         JComponent p = new PractiConcu();
-        p.setOpaque(true); //content panes must be opaque
+        p.setOpaque(true); 
         frame.setContentPane(p);
 
-        //Display the window.
         frame.pack();
         frame.setVisible(true);
     }
