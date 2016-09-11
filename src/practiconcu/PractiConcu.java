b0c3ac40af3925a9d70eee534fd311582bb3cc80
ChampionSean <marcoaurelio1655@ciencias.unam.mx>
2016-09-11 15:32:52 -0500

    ya listo falta borrar comentarios en ingles

--- src/practiconcu/PractiConcu.java
+++ src/practiconcu/PractiConcu.java
@@ -22,12 +22,19 @@ public class PractiConcu extends JPanel
 
     //Monitor de progreso
     private ProgressMonitor progressMonitor;
+    private ProgressMonitor progressMonitor2;
+    private ProgressMonitor progressMonitor3;
     //Botón que iniciará la ejecucuión
     private final JButton botonInicio;
     //Área para visualización del mensaje
     private final JTextArea parar;
     //Tarea a supervisar
     private Task tarea;
+    
+    private Task tarea2;
+    private Task tarea3;
+     private Thread t,h,i;        
+            
     
     class Task extends SwingWorker<Void, Void> {
 
@@ -36,10 +43,17 @@ public class PractiConcu extends JPanel
             Random random = new Random();
             int i = 0;
             setProgress(0);
+           Thread m = new Thread(new Runnable(){
+               public void run(){
+                  
+                }
+                   
+               
+            });
             try {
-                Thread.sleep(1000);
+                m.sleep(1000);
                 while (i < 100 && !isCancelled()) {
-                    Thread.sleep(random.nextInt(1000));
+                    m.sleep(random.nextInt(1000));
                     i += random.nextInt(10);
                     setProgress(Math.min(i, 100));
                 }
@@ -80,21 +94,42 @@ public class PractiConcu extends JPanel
         progressMonitor = new ProgressMonitor(PractiConcu.this,
                 "Ejecutando tarea",
                 "", 0, 100);
+        progressMonitor2 = new ProgressMonitor(PractiConcu.this,
+                "Ejecutando tarea",
+                "", 0, 100);
+        progressMonitor3 = new ProgressMonitor(PractiConcu.this,
+                "Ejecutando tarea",
+                "", 0, 100);
         progressMonitor.setProgress(0);
+        progressMonitor2.setProgress(0);
+        progressMonitor3.setProgress(0);
         tarea = new Task();
+        tarea2 = new Task();
+        tarea3 = new Task();
         tarea.addPropertyChangeListener(this);
+        tarea2.addPropertyChangeListener(this);
+        tarea3.addPropertyChangeListener(this);
         tarea.execute();
+        tarea2.execute();
+        tarea3.execute();
         botonInicio.setEnabled(false);
     }
 
     public void propertyChange(PropertyChangeEvent evt) {
         if ("progress".equals(evt.getPropertyName())) {
             int i = (Integer) evt.getNewValue();
-            progressMonitor.setProgress(i);
-            String m
-                    = String.format("Completado %d%%.\n", i);
+            progressMonitor.setProgress(tarea.getProgress());
+            progressMonitor2.setProgress(tarea2.getProgress());
+            progressMonitor3.setProgress(tarea3.getProgress());
+            String m= String.format("Completado proceso1 %d%%.\n", tarea.getProgress());
+            String n= String.format("Completado proceso2 %d%%.\n", tarea2.getProgress());
+            String k= String.format("Completado proceso3 %d%%.\n", tarea3.getProgress());
             progressMonitor.setNote(m);
+            progressMonitor2.setNote(n);
+            progressMonitor3.setNote(k);
             parar.append(m);
+            parar.append(n);
+            parar.append(k);
             if (progressMonitor.isCanceled() || tarea.isDone()) {
                 Toolkit.getDefaultToolkit().beep();
                 if (progressMonitor.isCanceled()) {
@@ -105,7 +140,9 @@ public class PractiConcu extends JPanel
                 }
                 botonInicio.setEnabled(true);
             }
+            
         }
+           
 
     }
 
